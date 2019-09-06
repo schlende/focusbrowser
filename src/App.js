e21a7ee8160d8cb2e67cdca0a58b831e623b5655
Walt Schlender <walt.schlender@gmail.com>
2019-09-06 20:59:58 +0800

    Basic browser works

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 import SessionStart from './components/SessionStart';
 import BrowserTab from './components/BrowserTab';
 import Timer from './components/Timer';
-import BrowseControls from './components/BrowseControls';
 
 function App() {
   return (
     <div className="App">
-      <BrowseControls />
       <BrowserTab />
       <Timer/>
     </div>
--- src/components/BrowserTab.js
+++ src/components/BrowserTab.js
@@ -8,15 +8,67 @@ const { BrowserWindow, BrowserView } = require('electron').remote;
 export default class BrowserTab extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { currentUrl: "https://www.google.com" }
   }
 
   componentDidMount() {
+    var view = this.browser();
+    view.addEventListener('did-finish-load', () => {
+      this.setState({currentUrl: view.src});
+    });
+  }
+
+  browser = () => {
+    return document.getElementById("browser");
+  }
+
+  handleBack = () => {
+    this.browser().goBack();
+  }
+
+  handleForward = () => {
+    this.browser().goForward();
+  }
+
+  handleReload = () => {
+    this.browser().reload();
+  }
+
+  _handleUrlValueChange = (event) => {
+    this.setState({currentUrl: event.target.value});
+  }
+
+  _handleUrlSubmit = (event) => {
+    event.preventDefault();
+
+    var url = this.state.currentUrl;
+
+    if(!url.includes('.')){
+      url = "https://www.google.com/search?q=" + url;
+    }else if (!/^https?:\/\//i.test(url)) {
+      url = 'http://' + url;
+    }
+
+    this.browser().src = url;
   }
 
   render() {
     return (
-      <div className="box browser-tab"> 
-        <webview src="https://google.com" autosize="on"></webview> 
+      <div className="browser-tab">
+        <span className="browser-controls">
+          <button onClick={this.handleBack}>Back</button>
+          <button onClick={this.handleForward}>Forward</button>
+          <button onClick={this.handleReload}>Reload</button>
+          <form onSubmit={this._handleUrlSubmit} className="url-form">
+            <input className="url-bar" type="text" value={this.state.currentUrl} onChange={this._handleUrlValueChange} />
+            <input type="submit" value="Go" />
+          </form>
+        </span>
+        <div className="browser-window">
+          <input type="text" />
+          <webview id="browser" src="https://google.com" autosize="on"></webview> 
+        </div>
       </div>
     )
   }
