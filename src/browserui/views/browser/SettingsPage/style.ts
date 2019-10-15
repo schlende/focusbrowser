4689fcc9281b760fb9eaa046e5bc7e7b601c9cb5
Walt Schlender <walt.schlender@gmail.com>
2019-10-15 12:59:55 +0800

    Changed the color of the text

--- src/browserui/views/browser/SettingsPage/style.ts
+++ src/browserui/views/browser/SettingsPage/style.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const SettingsHeader = styled('div')`
   background-color: #326BD2;
+  color: white;
   margin-top: 60px;
   width: 100%;
   height: 40px;
