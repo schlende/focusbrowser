69fcbe303dc92fd08e61aa7dc3d43412b2427d6c
王茂哲 <paulwang@wangmaozhede-MacBook-Pro.local>
2019-10-18 12:11:04 +0800

    Built the settings page UI

--- src/browserui/views/browser/SettingsPage/index.tsx
+++ src/browserui/views/browser/SettingsPage/index.tsx
@@ -1,13 +1,88 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { observer } from 'mobx-react-lite';
-import { SettingsHeader } from './style';
+import { SettingsHeader, Box1, Box2, Header1 ,LearnMoreButton, OptionLabel, OptionLabel2, Topic1, RadioButton } from './style';
 
 
 export const SettingsPage = observer(() => {
   return (
-    <SettingsHeader>
-      Settings page code goes here
-    </SettingsHeader>
+    <div>
+      <SettingsHeader>
+        Settings
+      </SettingsHeader>
+      <Box1>
+        Domain
+        <Box2>
+          <LearnMoreButton>
+              Learn More
+          </LearnMoreButton>
+          <Header1>  
+            Resolve domain via
+          </Header1>
+
+          <div>
+            <div>
+              <RadioButton type="radio" name="domain" value="1" />
+              <OptionLabel>Unstoppable.com API -</OptionLabel> <OptionLabel2> Non-parnoid + fast response times</OptionLabel2>
+            </div>
+            <div>
+              <RadioButton type="radio" name="domain" value="2" />
+              <OptionLabel>Zilliqua API -</OptionLabel> <OptionLabel2> Non-parnoid + fast response times</OptionLabel2>
+            </div>
+            <div>
+              <RadioButton type="radio" name="domain" value="3" />
+              <OptionLabel>Direct blockchain lookup -</OptionLabel> <OptionLabel2> Non-parnoid + slow response times</OptionLabel2>
+            </div>
+            <Topic1>
+              <OptionLabel>Read address from - </OptionLabel><input type="text" name="firstname" />
+            </Topic1>
+          </div>
+        </Box2>
+
+      </Box1>
+      <Box1>
+        IPFS Content
+        <Box2>
+          <LearnMoreButton>
+              Learn More
+          </LearnMoreButton>
+          <Header1>  
+            Download IPFS content via
+          </Header1>
+          
+          <div>
+            <div>
+              <RadioButton type="radio" name="content" value="1" />
+              <OptionLabel>Cloudlare CDN -</OptionLabel> <OptionLabel2> Non-parnoid + fast response times</OptionLabel2>
+            </div>
+            <div>
+              <RadioButton type="radio" name="content" value="2" />
+              <OptionLabel>Infura API -</OptionLabel> <OptionLabel2> Non-parnoid + fast response times</OptionLabel2>
+            </div>
+            <div>
+              <RadioButton type="radio" name="content" value="3" />
+              <OptionLabel>Designted IPFS Node -</OptionLabel> <OptionLabel2> Non-parnoid + slow response times</OptionLabel2>
+            </div>
+            <Topic1>
+              <div>
+                <RadioButton type="radio" name="content" value="4" />
+                <OptionLabel> Specify a node - </OptionLabel><input type="text" name="firstname" />
+              </div>
+              <div>
+                <RadioButton type="radio" name="content" value="5" />
+                <OptionLabel> Run your own local node(use more internet bandwidth) </OptionLabel>
+              </div>
+            </Topic1>
+            
+          </div>
+        </Box2>
+          
+
+      </Box1>
+    </div >
+    
+
+    
   );
 });
+
--- src/browserui/views/browser/SettingsPage/style.ts
+++ src/browserui/views/browser/SettingsPage/style.ts
@@ -1,9 +1,77 @@
 import styled, { css } from 'styled-components';
 
 export const SettingsHeader = styled('div')`
-  background-color: #326BD2;
+  box-sizing: border-box;
+  background-color: #0066FF;
   color: white;
   margin-top: 60px;
   width: 100%;
   height: 40px;
+  line-height: 40px;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
+
+export const Box1 = styled('div')`
+  line-height: 3;
+  margin-left: 10%;
+  margin-right: 10%;
+  font-size: 15px;
+  font-weight: 900;
+`;
+
+export const Header1 = styled('div')`
+  border-bottom: 1px solid #b5a5a5;
+  line-height: 3;
+  background-color: none;
+`;
+
+export const Box2 = styled('div')`
+  box-sizing: border-box;
+  line-height: 2;
+  padding-left: 30px;
+  width: 100%;
+  font-size: 15px;
+  font-weight: 900;
+  padding-bottom:12px;
+  background-color: none;
+`;
+
+
+export const LearnMoreButton = styled('div')`
+    display: inline-block;
+    user-select: none;
+    border: 3px solid #0000004a;
+    border-radius: 9px;
+    width: 88px;
+    position: relative;
+    cursor: pointer;
+    float: right;
+    font-size: 14px;
+    padding-left: 12px;
+    line-height: 2;
+    :hover{
+      border: 3px solid #888888;
+      background-color: #DDDDDD;
+    }
+  `;
+
+export const RadioButton = styled('input')`
+    margin-right: 10px;
+    
+    
+`;
+
+export const OptionLabel = styled('span')`
+  font-weight: bolder;
+  font-size: 15px;
+`;
+
+export const OptionLabel2 = styled('span')`
+  font-size: 15px;
+
+`; 
+export const Topic1 = styled('div')`
+  margin-left: 26px;
+  
 `;
