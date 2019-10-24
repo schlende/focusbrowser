933bbb711b162a6a40578f1ed5874a3e47835ad2
Walt Schlender <walt.schlender@gmail.com>
2019-10-24 16:40:47 +0800

    Domain resolver is working better...

--- src/browserui/mixins/domain-resolver.ts
+++ src/browserui/mixins/domain-resolver.ts
@@ -30,27 +30,27 @@ export class DomainResolver {
 
       if (extension == 'zil') {
         showUrl = url.replace('http://', 'ipfs://');
-        // DomainResolver.resolveZilBlockchain(domain).then((zilResult) => { })
+        DomainResolver.resolveZilBlockchain(domain).then((zilResult) => { })
 
-        DomainResolver.resolveZil(domain).then((zilResult) => {
-          if (zilResult) {
-            destUrl = "https://cloudflare-ipfs.com/ipfs/" + zilResult + "/";
-          } else {
-            if (showUrl.indexOf('ipfs://brad.zil') != -1) {
-              destUrl = "https://cloudflare-ipfs.com/ipfs/QmefehFs5n8yQcGCVJnBMY3Hr6aMRHtsoniAhsM1KsHMSe/";
-            } else if (showUrl.indexOf('ipfs://matt.zil') != -1) {
-              destUrl = "https://cloudflare-ipfs.com/ipfs/QmUD69diRF8jwju2k4b9mD7PaXMjtMAKafqascL18VKvoD/";
-            } else {
-              destUrl = "https://cloudflare-ipfs.com/ipfs/QmWcLKHWqrRB95zQnb4vX8RRgoGsVm5YAUHyZyiAw4mCMQ/";
-            }
-          }
+        // DomainResolver.resolveZil(domain).then((zilResult) => {
+        //   if (zilResult) {
+        //     destUrl = "https://cloudflare-ipfs.com/ipfs/" + zilResult + "/";
+        //   } else {
+        //     if (showUrl.indexOf('ipfs://brad.zil') != -1) {
+        //       destUrl = "https://cloudflare-ipfs.com/ipfs/QmefehFs5n8yQcGCVJnBMY3Hr6aMRHtsoniAhsM1KsHMSe/";
+        //     } else if (showUrl.indexOf('ipfs://matt.zil') != -1) {
+        //       destUrl = "https://cloudflare-ipfs.com/ipfs/QmUD69diRF8jwju2k4b9mD7PaXMjtMAKafqascL18VKvoD/";
+        //     } else {
+        //       destUrl = "https://cloudflare-ipfs.com/ipfs/QmWcLKHWqrRB95zQnb4vX8RRgoGsVm5YAUHyZyiAw4mCMQ/";
+        //     }
+        //   }
 
-          if (destUrl && showUrl) {
-            resolve({ url: showUrl, dest: destUrl });
-          } else {
-            reject("Bad show / dest url");
-          }
-        });
+        //   if (destUrl && showUrl) {
+        //     resolve({ url: showUrl, dest: destUrl });
+        //   } else {
+        //     reject("Bad show / dest url");
+        //   }
+        // });
 
 
       } else {
@@ -62,12 +62,18 @@ export class DomainResolver {
   public static resolveZilBlockchain(domain: string) {
     return new Promise((resolve, reject) => {
 
-      let dmn = 'resolver.eth';
+      let dmn = domain + '.zil';
 
       namicorn.address(dmn, 'ETH')
         .then((response:any) => {
           console.log(dmn, ' resolves to', response)
         }).catch(console.error)
+
+      namicorn.resolve(dmn)
+        .then((response:any) => {
+          console.log(dmn, ' has addresses ', response)
+        }).catch(console.error)
+      
     })
   }
 
