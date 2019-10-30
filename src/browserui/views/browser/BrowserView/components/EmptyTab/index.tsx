a9a3ecad0d8b288eb2a0c096a8ae4ed2768dba2e
Walt Schlender <walt.schlender@gmail.com>
2019-10-30 11:24:01 +0800

    Instructions for Wed!!

--- src/browserui/views/browser/BrowserView/components/EmptyTab/index.tsx
+++ src/browserui/views/browser/BrowserView/components/EmptyTab/index.tsx
@@ -5,6 +5,60 @@ import { SearchBar, SearchIcon } from './style';
 import { ButtonArea, Button1, TextLabel1 } from './style';
 
 
+/*
+Instructions for the day
+
+1. Make the UI look beatuful...
+* Hand cursor on hover
+* Size buttons correctly... just make it look nice
+2. Build out the structured data for the bookmarks
+3. Drive the UI using the structured data (if there are 6 bookmark elements... you should see them in the display... 
+should be laid out nicely)
+4. Make the buttons clickable...
+
+*/
+
+
+const clickHandler = () => {
+  console.log("Clicked!!!!");
+}
+
+var bookmarksData = [
+  { "name": "something", "url": "http://somethingelse.com" }
+]
+
+var array = [{
+  "name": "joe",
+  "age": 10,
+  "education": {
+    "highschool": "Fairview"
+  }
+},
+{
+  "name": "walt",
+  "age": 36,
+  "education": {
+    "highschool": "Fairview"
+  }
+}
+]
+
+console.log(array[1].education.highschool);
+
+
+const BookMarkButtons = () => {
+  var bookmarks = [];
+
+  for (var i = 0; i < bookmarksData.length; i++) {
+    console.log(bookmarksData[i].name);
+    bookmarks.push(
+      <div onClick={clickHandler}>a</div>
+    )
+  }
+
+  return bookmarks;
+}
+
 export const EmptyTab = observer(() => {
 
   return (
@@ -15,21 +69,10 @@ export const EmptyTab = observer(() => {
           <SearchIcon />
         </SearchBar>
         <ButtonArea>
+          {BookMarkButtons()}
           <div>
             <Button1 />
             <TextLabel1>Description1</TextLabel1>
-          </div>
-          <div>
-            <Button1 />
-            <TextLabel1>Description2</TextLabel1>
-          </div>
-          <div>
-            <Button1 />
-            <TextLabel1>Description3</TextLabel1>
-          </div>
-          <div>
-            <Button1 />
-            <TextLabel1>Description4</TextLabel1>
           </div>
         </ButtonArea>
       </Container>
